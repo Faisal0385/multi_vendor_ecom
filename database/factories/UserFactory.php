a0a6f1945aaff2f4b8cc0cef2a8483ed57fc938f
Personal <faisaltez@gmail.com>
2023-06-10 22:20:19 +0600

    Laravel Multi Auth with Breeze Part 1

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,15 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            
+            ## Added these new lines start
+            'phone'   => fake()->phoneNumber,
+            'address' => fake()->address,
+            'photo'   => fake()->imageUrl('60','60'),
+            'role'    => fake()->randomElement(['admin','vendor','user']),
+            'status'  => fake()->randomElement(['active','inactive']),
+            ## Added these new lines end
+
             'remember_token' => Str::random(10),
         ];
     }
